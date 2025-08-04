25573ff961ec73e267e2b19a3071a2e9f89161a3
Paul Verdure <paul.verdure@pon.com>
2025-08-04 13:21:08 +0200

    Refactor code structure for improved readability and maintainability

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  plugins: [
+    '@trivago/prettier-plugin-sort-imports',
+    'prettier-plugin-tailwindcss',
+  ],
+  importOrder: ['^react$', '<THIRD_PARTY_MODULES>', '^@/(.*)$', '^[./]'],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+}
